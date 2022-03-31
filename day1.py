e7be30185d363299ddce5fe641c6dfcc79c13b62
ken020589 <102386168+ken020589@users.noreply.github.com>
2022-04-01 01:21:04 +0800

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+print('hello world')
+print('ken')
+ken = "Ken Lu"
+print(ken)
+del ken
+age, ken = 22, "Ken Lu"
+print(age,ken)
+print("age %d my name is %s" % (age, ken))
+print("age{} my name is {}".format(age, ken))
